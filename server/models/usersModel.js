56c97926792b201694b60ca1bdfe44261aca7c95
HasanOmar1 <r3domar1@gmail.com>
2024-04-13 15:47:55 +0300

    added hoodies model

--- server/models/usersModel.js
+++ server/models/usersModel.js
@@ -1,22 +1,31 @@
 import mongoose from "mongoose";
 
-const usersSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide a name"],
-    unique: true,
+const usersSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minLength: [2, "Please provide at least two characters"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Please provide an email"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide a password"],
-    minLength: [2, "Please provide at least two characters"],
-  },
-});
+  {
+    toJSON: {
+      transform(_, ret) {
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 const User = mongoose.model("Users", usersSchema);
 export default User;
