a72951071f38977a6c5c017c21d9921a5c2dd339
HasanOmar1 <r3domar1@gmail.com>
2024-04-28 22:13:59 +0300

    commented out bagRoute and BagController

--- server/controllers/BagController.js
+++ server/controllers/BagController.js
@@ -1,93 +1,93 @@
-import STATUS_CODE from "../constants/statusCodes.js";
-import BagItems from "../models/bagItemsModel.js";
-import Bag from "../models/bagModel.js";
-import User from "../models/usersModel.js";
+// import STATUS_CODE from "../constants/statusCodes.js";
+// import BagItems from "../models/bagItemsModel.js";
+// import Bag from "../models/bagModel.js";
+// import User from "../models/usersModel.js";
 
-export const getAllBag = async (req, res, next) => {
-  try {
-    const bag = await Bag.find({}).populate("clothes");
-    res.send(bag);
-  } catch (error) {
-    next(error);
-  }
-};
+// export const getAllBag = async (req, res, next) => {
+//   try {
+//     const bag = await Bag.find({}).populate("clothes");
+//     res.send(bag);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-export const addToBag = async (req, res, next) => {
-  try {
-    const { clothes } = req.body;
-    if (!clothes) {
-      res.status(STATUS_CODE.BAD_REQUEST);
-      throw new Error("Clothe's id is missing");
-    }
-    const newBag = await Bag.create({
-      clothes,
-      user: req.user._id,
-    });
+// export const addToBag = async (req, res, next) => {
+//   try {
+//     const { clothes } = req.body;
+//     if (!clothes) {
+//       res.status(STATUS_CODE.BAD_REQUEST);
+//       throw new Error("Clothe's id is missing");
+//     }
+//     const newBag = await Bag.create({
+//       clothes,
+//       user: req.user._id,
+//     });
 
-    const updatedUser = await User.findByIdAndUpdate(
-      req.user._id,
-      { $push: { bag: newBag } },
-      { new: true }
-    ).populate({
-      path: "bag",
-      populate: {
-        path: "clothes",
-      },
-    });
+//     const updatedUser = await User.findByIdAndUpdate(
+//       req.user._id,
+//       { $push: { bag: newBag } },
+//       { new: true }
+//     ).populate({
+//       path: "bag",
+//       populate: {
+//         path: "clothes",
+//       },
+//     });
 
-    res.status(STATUS_CODE.CREATED);
-    res.send(updatedUser);
-  } catch (error) {
-    next(error);
-  }
-};
+//     res.status(STATUS_CODE.CREATED);
+//     res.send(updatedUser);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-export const deleteBag = async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const bag = await Bag.findByIdAndDelete(id);
-    if (!bag) {
-      res.status(STATUS_CODE.NOT_FOUND);
-      throw new Error("Bag id not found");
-    }
-    const updatedUser = await User.findByIdAndUpdate(
-      req.user._id,
-      { $pull: { bag: id } },
-      { new: true }
-    ).populate({
-      path: "bag",
-      populate: {
-        path: "clothes",
-      },
-    });
-    res.send(updatedUser);
-  } catch (error) {
-    next(error);
-  }
-};
+// export const deleteBag = async (req, res, next) => {
+//   const { id } = req.params;
+//   try {
+//     const bag = await Bag.findByIdAndDelete(id);
+//     if (!bag) {
+//       res.status(STATUS_CODE.NOT_FOUND);
+//       throw new Error("Bag id not found");
+//     }
+//     const updatedUser = await User.findByIdAndUpdate(
+//       req.user._id,
+//       { $pull: { bag: id } },
+//       { new: true }
+//     ).populate({
+//       path: "bag",
+//       populate: {
+//         path: "clothes",
+//       },
+//     });
+//     res.send(updatedUser);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-export const checkout = async (req, res, next) => {
-  try {
-    const updateUser = await User.updateMany(
-      { _id: req.user._id },
-      {
-        $unset: { bag: 1 },
-      }
-    );
+// export const checkout = async (req, res, next) => {
+//   try {
+//     const updateUser = await User.updateMany(
+//       { _id: req.user._id },
+//       {
+//         $unset: { bag: 1 },
+//       }
+//     );
 
-    res.send(updateUser);
-  } catch (error) {
-    next(error);
-  }
-};
+//     res.send(updateUser);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-export const getBagHistory = async (req, res, next) => {
-  try {
-    const bagHistory = await Bag.find({ user: req.user._id }).populate(
-      "clothes"
-    );
-    res.send(bagHistory);
-  } catch (error) {
-    next(error);
-  }
-};
+// export const getBagHistory = async (req, res, next) => {
+//   try {
+//     const bagHistory = await Bag.find({ user: req.user._id }).populate(
+//       "clothes"
+//     );
+//     res.send(bagHistory);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
--- server/routes/bagRoute.js
+++ server/routes/bagRoute.js
@@ -1,19 +1,19 @@
 import express from "express";
-import {
-  addToBag,
-  checkout,
-  deleteBag,
-  getAllBag,
-  getBagHistory,
-} from "../controllers/BagController.js";
-import protect from "../middlewares/authMiddleware.js";
+// import {
+//   addToBag,
+//   checkout,
+//   deleteBag,
+//   getAllBag,
+//   getBagHistory,
+// } from "../controllers/BagController.js";
+// import protect from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", getAllBag);
-router.get("/history", protect, getBagHistory);
-router.post("/add", protect, addToBag);
-router.delete("/delete/:id", protect, deleteBag);
-router.delete("/checkout", protect, checkout);
+// router.get("/", getAllBag);
+// router.get("/history", protect, getBagHistory);
+// router.post("/add", protect, addToBag);
+// router.delete("/delete/:id", protect, deleteBag);
+// router.delete("/checkout", protect, checkout);
 
 export default router;
