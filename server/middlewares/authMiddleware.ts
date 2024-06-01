04c088c0646600ec9249708d7bdd86fd1770a98f
HasanOmar1 <r3domar1@gmail.com>
2024-06-01 13:14:16 +0300

    fixed auth middleware to not break server if no token

--- server/middlewares/authMiddleware.ts
+++ server/middlewares/authMiddleware.ts
@@ -7,36 +7,40 @@ import { AuthenticatedRequest } from "../utils/AuthenticatedRequest.js";
 
 const protect = asyncHandler(
   async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
-    let token: string | undefined;
+    try {
+      let token: string | undefined;
 
-    const authHeader = req.headers.authorization || req.headers.Authorization;
-    if (
-      authHeader &&
-      typeof authHeader === "string" &&
-      authHeader.startsWith("Bearer")
-    ) {
-      token = authHeader.split(" ")[1];
-      if (!process.env.JWT_SECRET) {
+      const authHeader = req.headers.authorization || req.headers.Authorization;
+      if (
+        authHeader &&
+        typeof authHeader === "string" &&
+        authHeader.startsWith("Bearer")
+      ) {
+        token = authHeader.split(" ")[1];
+        if (!process.env.JWT_SECRET) {
+          res.status(STATUS_CODE.FORBIDDEN);
+          throw new Error("JWT_SECRET is not defined");
+        }
+
+        try {
+          const decoded: any = jwt.verify(token, process.env.JWT_SECRET);
+          req.user = await User.findById(decoded.id).select("-password");
+
+          next();
+        } catch (error) {
+          console.error("Token Verification error: " + error);
+          res
+            .status(STATUS_CODE.FORBIDDEN)
+            .send("You must be logged in to do this action");
+        }
+      }
+
+      if (!token) {
         res.status(STATUS_CODE.FORBIDDEN);
-        throw new Error("JWT_SECRET is not defined");
+        throw new Error("You must be logged in to do this action");
       }
-
-      try {
-        const decoded: any = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await User.findById(decoded.id).select("-password");
-
-        next();
-      } catch (error) {
-        console.error("Token Verification error: " + error);
-        res
-          .status(STATUS_CODE.FORBIDDEN)
-          .send("You must be logged in to do this action");
-      }
-    }
-
-    if (!token) {
-      res.status(STATUS_CODE.FORBIDDEN);
-      throw new Error("You must be logged in to do this action");
+    } catch (error) {
+      next(error);
     }
   }
 );
