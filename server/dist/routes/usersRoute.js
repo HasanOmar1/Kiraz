ddf12ba02166c95c9d87359366966de05c6bf00f
HasanOmar1 <r3domar1@gmail.com>
2024-07-26 17:43:39 +0300

    added dist to .gitignore

--- server/dist/routes/usersRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createUser, deleteUser, getAllUsers, login, } from "../controllers/usersController.js";
-const router = express.Router();
-router.get("/", getAllUsers);
-router.post("/create", createUser);
-router.post("/login", login);
-router.delete("/delete/:id", deleteUser);
-router.delete("/delete", deleteUser);
-export default router;
--- server/dist/utils/capitalLetter.js
+++
@@ -1,3 +0,0 @@
-export const capitalLetter = (name) => {
-    return name[0].toUpperCase() + name.slice(1);
-};
